f0eed133c813988ef1b396b26e96b628c0743e74
updpujan <updpujan1@gmail.com>
2026-09-16 16:26:09 +0545

    updated: registration route

--- src/Routes/authRoute.ts
+++ src/Routes/authRoute.ts
@@ -1,11 +1,16 @@
 import express from "express";
 import {tokenGeneration} from "../utils/jwt.js";
+import protect from "../middleware/authMiddleware.js"
+import { validateRegistration } from "../middleware/validationMiddleware.js";
 
 const route = express.Router();
 
-route.get("/",(req,res)=>{
+route.get("/login",(req,res)=>{
     const token = tokenGeneration(123);
     res.json({token,});
 });
+
+route.post("/registration",validateRegistration);
+
 
 export default route;
