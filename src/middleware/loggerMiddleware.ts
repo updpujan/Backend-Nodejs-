3e2fe885f3691224c4f3a3a86397f26511b64b1e
updpujan <updpujan1@gmail.com>
2026-09-16 09:38:22 +0545

    update: log ip,method,timestamp

--- src/middleware/loggerMiddleware.ts
+++ src/middleware/loggerMiddleware.ts
@@ -1,10 +1,28 @@
 import { Request,Response,NextFunction } from "express";
+import fs from "node:fs";
+
 
 const loggerMiddleware = (req:Request,res:Response,next:NextFunction)=>{
-    console.log(`${req.method} ${req.url}`);
-    return res.status(403).json({
-    message: "Blocked by middleware"
-});
+
+    const timeStamp = Date.now();
+    const method = req.method;
+    const url = req.originalUrl;
+    const ip = req.ip;
+
+    const logPath = "/home/pujan-upd/Desktop/Backend(Nodejs)/backend/log/system.log";
+
+
+    if(fs.existsSync(logPath)){
+        fs.appendFile(logPath,`${timeStamp}: ip"${ip}" : ${method}-${url}\n`,(err)=>{
+            if(err) console.log("Failed to Append log:"+err);
+        });
+    }else{
+        fs.writeFile(logPath,`${timeStamp}: ip"${ip}" : ${method}-${url}\n`,(err)=>{
+            if(err) console.log("Failed to write log:"+err);
+        });
+    }
+    next();
+
 };
 
 export default loggerMiddleware
