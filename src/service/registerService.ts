129d05ca574dc3e5796f4917a43a676bb2f4aa5d
updpujan <updpujan1@gmail.com>
2026-09-16 16:11:54 +0545

    logic of registration service

---
+++ src/service/registerService.ts
@@ -0,0 +1,14 @@
+import bcrypt from "bcrypt";
+import { User } from "../model/userModel.js";
+import { getUsers,addUser } from "../repository/fileConnection.js";
+
+export const registerService = async (user:User)=>{
+    const usersCount = getUsers().length;
+    user.id = usersCount + 1;
+    const haspasword = await bcrypt.hash(user.password,10);
+    user.password = haspasword;
+    addUser(user);
+    return {sucess:true,
+        message: "User Created Sucessfully"
+    };
+};
